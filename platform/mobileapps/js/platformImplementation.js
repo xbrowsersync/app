e19cb9c47253f59fe355a5711813e4e189209fdd
nero120 <rich.dmz@protonmail.com>
2017-06-28 11:28:04 +0100

    Updated: Help text.

--- platform/mobileapps/js/platformImplementation.js
+++ platform/mobileapps/js/platformImplementation.js
@@ -50,16 +50,16 @@ xBrowserSync.App.PlatformImplementation = function($http, $interval, $q, $timeou
 			"message": "<h4>Welcome</h4><p>Thanks for using xBrowserSync — browser syncing as it should be: secure, anonymous and free!</p><p>Take some time to read through the following pages to get aquainted with xBrowserSync’s features. Futher information is available in the <a href='https://www.xbrowsersync.org/#faqs' class='new-tab'>FAQs</a>.</p>"
 		},
 		"login_introPanel2_Message": {
-			"message": "<h4>Syncing for the first time</h4><p>Your data is encrypted before it is synced so you’ll need to enter an encryption password first. Make it strong but also memorable, if you forget it you won’t be able to retrieve your synced data (and there are no reminders!).</p><p>Then simply click the Sync button and that’s it — you’re synced and ready to start adding bookmarks.</p>"
+			"message": "<h4>Syncing for the first time</h4><p>Before you can sync your data it needs to be encrypted so that only you can read it. Enter an encryption password — make it strong but also memorable, there are no resets or reminders so if you forget it you won’t be able to decrypt your synced data.</p><p>Once you click the Sync button you’re synced and ready to start adding bookmarks.</p>"
 		},
 		"login_introPanel3_Message": {
-			"message": "<h4>Already synced (got an ID)</h4><p>When you create a new sync you are given a unique xBrowserSync ID which you can use along with your password to sync your data on other devices.</p><p>Once synced you can view your ID in the Settings panel. Tap it to reveal a handy QR code to scan when syncing on mobile devices.</p>"
+			"message": "<h4>Already synced (got an ID)</h4><p>When you create a new sync you are given a unique xBrowserSync ID which you can use along with your password to sync your data on other devices. Your anonymity is ensured as no personal data is collected or stored along with your ID.</p><p>Once synced you can view your ID in the Settings panel. Tap it to reveal a handy QR code to scan when syncing on mobile devices.</p>"
 		},
 		"login_introPanel4_Message": {
 			"message": "<h4>Syncing to another service</h4><p>By default your data is synced to the official xBrowserSync service, though anyone can <a href='https://github.com/xBrowserSync/API' class='new-tab'>run their own xBrowserSync service</a>, either for private use (for ultimate security and privacy) or to make available for public use so that more people can enjoy xBrowserSync.</p><p>Check the available <a href='https://www.xbrowsersync.org/#status' class='new-tab'>xBrowserSync services</a> and switch services in the Settings panel.</p>"
 		},
 		"login_introPanel5_Message": {
-			"message": "<h4>New service, new ID</h4><p>Your xBrowserSync ID will only work with the service on which it was first synced.</p><p>Whenever you change services you must create a new sync and receive a new ID. Don’t worry, it’s easy to move your synced data by backing it up using the former service and simply restoring it on the latter.</p>"
+			"message": "<h4>New service, new ID</h4><p>Your xBrowserSync ID will only work with the service on which it was first synced.</p><p>Whenever you change services you must create a new sync and receive a new ID. It’s easy to move your synced data by backing it up using the former service and simply restoring it on the latter.</p>"
 		},
 		"login_introPanel6_Message": {
 			"message": "<h4>Searching your bookmarks</h4><p>Once synced, your bookmarks are displayed in chronological order when you open xBrowserSync. Type some keywords or a URL in the search box to search your bookmarks.</p><p>Long pressing on a bookmark will allow you to directly share, modify or delete the bookmark.</p>"
@@ -68,7 +68,7 @@ xBrowserSync.App.PlatformImplementation = function($http, $interval, $q, $timeou
 			"message": "<h4>Adding a bookmark</h4><p>Add bookmarks easily by sharing to xBrowserSync from any apps that share URLs such as browsers, YouTube, Spotify and many more.</p><p>The bookmark’s properties will be fetched for you, otherwise add a description and some tags to ensure better search results.</p>"
 		},
 		"login_introPanel8_Message": {
-			"message": "<h4>Remember to back up</h4><p>Nothing in life is 100% certain (especially web servers) so please look after your data and make sure to take backups.</p><p>You can do this easily in the Settings panel, you can back up your unencrypted data to a local file which can then restored at a later date should you need to.</p>"
+			"message": "<h4>Remember to back up</h4><p>When you use xBrowserSync your data is your reponsibility so be smart and make sure to take backups.</p><p>You can do this easily in the Settings panel, you can back up your unencrypted data to a local file which can then restored at a later date should you need to.</p>"
 		},
 		"login_introPanel9_Message": {
 			"message": "<h4>Got desktop?</h4><p>Sync your xBrowserSync data with your desktop browser using the xBrowserSync web extension, currently available for Chrome and with Firefox support coming very soon.</p>"
