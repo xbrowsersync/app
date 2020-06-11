9b2b4f8d7eabcbc6f0548da79910c566fee73055
nero120 <rich.dmz@protonmail.com>
2020-06-11 14:57:11 +0100

    Updated cordova build process to use custom gradle properties.

--- res/android/scripts/beforeBuild.js
+++ res/android/scripts/beforeBuild.js
@@ -2,7 +2,15 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = function (ctx) {
-  const source = path.join(ctx.opts.projectRoot, 'build-extras.gradle');
-  const destination = path.join(ctx.opts.projectRoot, 'platforms/android/app/build-extras.gradle');
+  let source, destination;
+  
+  // Copy build-extras.gradle
+  source = path.join(ctx.opts.projectRoot, 'build-extras.gradle');
+  destination = path.join(ctx.opts.projectRoot, 'platforms/android/app/build-extras.gradle');
+  fs.copyFileSync(source, destination);
+
+  // Copy gradle.properties
+  source = path.join(ctx.opts.projectRoot, 'gradle.properties');
+  destination = path.join(ctx.opts.projectRoot, 'platforms/android/app/gradle.properties');
   fs.copyFileSync(source, destination);
 };
