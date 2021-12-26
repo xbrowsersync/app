ed51e28227e422140fde5ba5fa48ea8e4caab2ac
nero120 <rich.dmz@protonmail.com>
2021-12-26 03:14:43 +0000

    Updated android packaging process.

--- scripts/create-apk-package.js
+++ scripts/create-apk-package.js
@@ -5,27 +5,20 @@ const path = require('path');
 const version = fs.readFileSync(path.resolve(__dirname, '../PACKAGE_VERSION'), 'utf8');
 const pathToApk = path.resolve(
   __dirname,
-  '../build/android/platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk'
+  '../build/android/platforms/android/app/build/outputs/apk/release/app-release.apk'
 );
 const outputDir = path.resolve(__dirname, '../dist');
-const alignedFilePath = `${outputDir}/xbrowsersync_${version}_android.apk`;
-const signedFilePath = `${outputDir}/xbrowsersync_${version}_android_signed.apk`;
-const unsignedFilePath = `${outputDir}/xbrowsersync_${version}_android_unsigned.apk`;
-const pathToKeyStore = path.resolve(__dirname, '../build/android/xbs_release_test.keystore');
-const signingAlias = 'xbs_release_test';
-const signingPassword = 'xbs_release_test';
+const outputFilePath = `${outputDir}/xbrowsersync_${version}_android.apk`;
+const pathToKeyStore = path.resolve(__dirname, '../build/android/xbrowsersync.keystore');
+const signingAlias = process.env.SIGNING_ALIAS;
+const signingPassword = process.env.SIGNING_PASSWORD;
+const signingStorePassword = process.env.SIGNING_STORE_PASSWORD;
 
 if (fs.existsSync(pathToApk)) {
   fs.unlinkSync(pathToApk);
 }
-if (fs.existsSync(alignedFilePath)) {
-  fs.unlinkSync(alignedFilePath);
-}
-if (fs.existsSync(signedFilePath)) {
-  fs.unlinkSync(signedFilePath);
-}
-if (fs.existsSync(unsignedFilePath)) {
-  fs.unlinkSync(unsignedFilePath);
+if (fs.existsSync(outputFilePath)) {
+  fs.unlinkSync(outputFilePath);
 }
 
 const runCommand = (command, cwd, callback) => {
@@ -49,22 +42,15 @@ const runCommand = (command, cwd, callback) => {
   });
 };
 
-runCommand('cordova build android --release', './build/android', () => {
-  if (!fs.existsSync(outputDir)) {
-    fs.mkdirSync(outputDir);
+runCommand(
+  `cordova build android --release -- --keystore=${pathToKeyStore} --storePassword=${signingStorePassword} --alias=${signingAlias} --password=${signingPassword}`,
+  './build/android',
+  () => {
+    if (!fs.existsSync(outputDir)) {
+      fs.mkdirSync(outputDir);
+    }
+    if (fs.existsSync(pathToApk)) {
+      fs.copyFileSync(pathToApk, outputFilePath);
+    }
   }
-  if (fs.existsSync(pathToApk)) {
-    fs.copyFileSync(pathToApk, signedFilePath);
-    fs.copyFileSync(pathToApk, unsignedFilePath);
-
-    runCommand(
-      `jarsigner -verbose -keystore ${pathToKeyStore} -storepass ${signingPassword} ${signedFilePath} ${signingAlias}`,
-      `${process.env.JAVA_HOME}/bin`,
-      () => {
-        runCommand(`zipalign -v 4 ${signedFilePath} ${alignedFilePath}`, null, () => {
-          fs.unlinkSync(signedFilePath);
-        });
-      }
-    );
-  }
-});
+);
